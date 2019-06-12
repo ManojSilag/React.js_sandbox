0733c9a07ccbbba92f6234a6c3358439dcdf9904
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-06-12 12:53:30 +0530

    creating seprate component for comments

--- 1section/components/src/index.js
+++ 1section/components/src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href='/' className="avatar">
-                    <img alt="avtar"/>
-                </a>
-                <div className="content">
-                <a href="/" className="author">
-                    SAM
-                </a>
-                </div>
-                <div className="metadata">
-                    <span className="data">Today at 6:00PM</span>
-                </div>
-                <div className="text">Nice blog post!</div>
-            </div>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
         </div>
     )
 }
