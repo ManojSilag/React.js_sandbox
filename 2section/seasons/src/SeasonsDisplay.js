6074647318395f7cd0eee2bd39b88e7b9be00bb7
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-06-15 19:37:52 +0530

    Starting new project pics

--- 2section/seasons/src/SeasonsDisplay.js
+++ 2section/seasons/src/SeasonsDisplay.js
@@ -1,8 +1,49 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonsDisplay = () => {
 
-    return <div>My name is winter</div>
+const seasonConfig = {
+
+    Winter :  {
+    text : "Burr, it is chilly",
+    iconName : 'snowflake'
+    },
+    Summer :  {
+    text : "Its time to hit Beach",
+    iconName : 'sun'
+    }
+}
+
+
+const getSeason = (lat, month) => {
+    if( month > 2 && month < 9){
+        return lat > 0  ? 'Summer' : 'Winter';
+    }
+    else{
+        return lat > 0 ? 'Winter' : 'Summer';
+    }
+}
+
+const SeasonsDisplay = (props) => {
+
+    const season = getSeason(props.lat, new Date().getMonth())
+    // const text = season === 'Winter' ? 'Burr, it is chilly' : 'Its time to hit Beach';
+    // const icon = season === 'Winter' ? 'snowflake' : 'sun';
+    
+    // console.log(seasonConfig);
+    // console.log(season);  
+    // console.log(seasonConfig[season]);
+
+    const { text, iconName } = seasonConfig[season];
+    
+    
+    return  (
+        <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`}/>
+        <h1 className='text'>{text}</h1>
+        <i className={`icon-right massive ${iconName} icon`}/>
+        </div>
+    )
 
 }
 
---
+++ 3section/pics/src/components/SearchBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+class SearchBox extends React.Component{
+    render(){
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image search</label>
+                        <input type="text"/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+
+
+export default SearchBox;
