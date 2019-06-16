2e2eee1acce6b44c8e8dca14fe55e6999845ef9a
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-06-16 13:38:19 +0530

    Handling user input with forms and events

--- 3section/pics/src/components/SearchBox.js
+++ 3section/pics/src/components/SearchBox.js
@@ -3,13 +3,27 @@ import React from 'react';
 
 
 class SearchBox extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = { term: ''}
+    }
+
+    onSubmitChange = (event) =>{
+        event.preventDefault();
+        
+        this.props.onSubmitt(this.state.term);
+    }
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onSubmitChange}>
                     <div className="field">
-                        <label>Image search</label>
-                        <input type="text"/>
+                        <label style={{display: 'flex', justifyContent: 'center'}}>Image search</label>
+                        <input 
+                        type="text" 
+                        value={this.state.term}
+                        onChange={ e => this.setState({ term: e.target.value}) }/>
                     </div>
                 </form>
             </div>
