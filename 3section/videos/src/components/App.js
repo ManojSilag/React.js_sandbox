46517f93afb1c2a9811a7bb9c23613a50223995f
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-06-22 21:47:22 +0530

    finishing the app

--- 3section/videos/src/components/App.js
+++ 3section/videos/src/components/App.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
+
     constructor(props){
         super(props)
+        this.state = { videos: [] , selectedVideo: null }
+    }
 
-        this.state = { videos: [] }
+    componentDidMount(){
+        this.OnTermSubmit('car')
     }
 
     OnTermSubmit =async term=> {
-        
         const response = await youtube.get('/search',{
             params:{ q: term },
         });
-
         //console.log(response);
-        this.setState({ videos: response.data.items })
-        //console.log(response.data.items);
-        
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0]})
+        //console.log(response.data.items); 
+    }
+
+    onVideoSelect = (video)=>{
+        //console.log( video, 'from the apppp');
+        this.setState({ selectedVideo: video })
     }
     render(){
         return (
             <div className="ui container">
             <SearchBar onFormSubmitt={this.OnTermSubmit}/>
-            <VideoList videos={this.state.videos}/>
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="ten wide column">
+                        <VideoDetail video={this.state.selectedVideo}/>
+                    </div>
+                    <div className="six wide column">
+                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                    </div>
+                </div>
+            </div>
             </div>
         )
     }
