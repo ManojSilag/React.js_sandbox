17c010a6356e3fafc878bd100e13d711c362127f
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-07-07 19:25:39 +0530

    working with redux thunk

--- 4section/blog/src/index.js
+++ 4section/blog/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducer from './reducers';
 
+const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDom.render(
-    <Provider store={createStore(reducer)}>
+    <Provider store={store}>
     <App/>
     </Provider>,
     document.querySelector('#root')
