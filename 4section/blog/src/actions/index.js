575417983d420946e254b6c870803b6cb7e041e9
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-07-08 21:03:58 +0530

    working with apis

--- 4section/blog/src/actions/index.js
+++ 4section/blog/src/actions/index.js
@@ -2,5 +2,5 @@ import Jsonplaceholder from '../apis/Jsonplaceholder';
 
 export const fetchPosts = () =>   async dispatch => {
         const response =  await Jsonplaceholder.get('/posts');
-        dispatch({type: 'FETCH_POST',payload: response})
+        dispatch({type: 'FETCH_POST',payload: response.data})
     };
