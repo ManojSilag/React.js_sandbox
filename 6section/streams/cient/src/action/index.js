2ba4bb238e66d5a40e11bea6ba295aa1673a6aa3
ManojSilag <manojsilag@gmail.com>
2020-02-29 19:00:53 +0530

    reuse

--- 6section/streams/cient/src/action/index.js
+++ 6section/streams/cient/src/action/index.js
@@ -8,6 +8,7 @@ import {
   EDIT_STREAM
 } from "./types";
 import streams from "../apis/stream";
+import history from "../history";
 
 export const signIn = (userId) => {
   return {
@@ -23,9 +24,11 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", {...formValues, userId});
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
