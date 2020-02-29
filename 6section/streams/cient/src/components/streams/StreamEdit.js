fc494b6225c3d8d6166de3ff403a1305d5e2ef94
ManojSilag <manojsilag@gmail.com>
2020-02-29 21:46:54 +0530

    REST-Based React Apps

--- 6section/streams/cient/src/components/streams/StreamEdit.js
+++ 6section/streams/cient/src/components/streams/StreamEdit.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../action";
+import { fetchStream, editStream } from "../../action";
+import StreamForm from "./StreamForm";
+import _ from 'lodash';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+    this.props.editStream(this.props.match.params.id,formValues)
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>...Loading</div>;
     }
-    return <div>Edit</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -22,5 +37,6 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(mapStateToProps, {
-  fetchStream
+  fetchStream,
+  editStream
 })(StreamEdit);
