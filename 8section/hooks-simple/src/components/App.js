afc4d5ce449619b7dd2adafe848324856969db16
ManojSilag <manojsilag@gmail.com>
2020-03-07 13:37:32 +0530

    hooks

---
+++ 8section/hooks-simple/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const App = () => {
+
+    const [resource, setResource ] = useState('posts');
+    
+  return (
+    <div className="ui container">
+      <div>
+        <button onClick={()=> setResource('posts')}>Posts</button>
+        <button onClick={()=> setResource('todos')}>todos</button>
+      </div>
+      {resource}
+    </div>
+  );
+};
+
+export default App;
+
